0eab529607a68949f9ba62474d4028ba63ca010d
Simon Fraser <sfraser@mozilla.com>
2018-08-28 13:23:20 +0100

    Find by revision

--- scripts/costings.py
+++ scripts/costings.py
@@ -1,8 +1,11 @@
 
 import csv
 import asyncio
+import argparse
 from datetime import datetime, timedelta
 from collections import defaultdict
+
+import taskcluster.aio as taskcluster
 
 from taskhuddler.aio.graph import TaskGraph
 
@@ -15,9 +18,48 @@ def fetch_worker_costs(year, month):
         return {row[1]: float(row[4]) for row in reader}
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="CI Costs")
+    parser.add_argument('--taskgroupid', type=int)
+    parser.add_argument('--revision', type=str)
+    parser.add_argument('--nightly', action='store_true')
+    parser.add_argument('--project', type=str, default='mozilla-central')
+    parser.add_argument('--product', type=str, default='firefox')
+    return parser.parse_args()
+
+
+async def find_taskgroup_by_revision(args):
+
+    if args.nightly:
+        nightly_index = "nightly."
+    else:
+        nightly_index = ""
+    index = "gecko.v2.{project}.{nightly}revision.{revision}.{product}.linux64-opt".format(
+        project=args.project,
+        nightly=nightly_index,
+        revision=args.revision,
+        product=args.product
+    )
+    print(index)
+    idx = taskcluster.Index()
+    queue = taskcluster.Queue()
+    build_task = await idx.findTask(index)
+    task_def = await queue.task(build_task['taskId'])
+
+    return task_def['taskGroupId']
+
+
 async def async_main():
     """Run all the async tasks."""
-    graph = await TaskGraph('b-ZKU1Q3QcKp3p7prOlURw')
+    args = parse_args()
+    if args.taskgroupid:
+        graph = await TaskGraph(args.taskgroupid)
+    elif args.revision:
+        graph_id = await find_taskgroup_by_revision(args)
+        graph = await TaskGraph(graph_id)
+
+
+    # graph = await TaskGraph('b-ZKU1Q3QcKp3p7prOlURw')
     total_wall_time_buckets = defaultdict(timedelta)
     for task in graph.tasks():
         if task.completed:
