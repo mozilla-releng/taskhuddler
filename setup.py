c713c0b3a492c9ce31bbf4548fab5bbdb741a9e4
Simon Fraser <srfraser@users.noreply.github.com>
2019-02-20 16:52:52 +0000

    Pypi readiness (#13)
    pypi upload preparations

--- setup.py
+++ setup.py
@@ -1,75 +1,41 @@
 from __future__ import print_function
 import json
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 import sys
 
 
-reqs = [
-    "virtualenv",
-    "asyncinit",
-    "taskcluster",
-    's3fs',
-    "python-dateutil",
-]
+project_dir = os.path.abspath(os.path.dirname(__file__))
 
-tests_require = [
-    "tox",
-    "virtualenv",
-    "asyncinit",
-    "taskcluster",
-    "python-dateutil",
-]
-
-PATH = os.path.join(os.path.dirname(__file__), "version.json")
-with open(PATH) as filehandle:
+with open(os.path.join(project_dir, 'version.json')) as filehandle:
     VERSION = json.load(filehandle)['version_string']
 
+with open(os.path.join(project_dir, 'requirements/main.in')) as filehandle:
+    requirements_raw = filehandle.readlines()
 
-class Tox(TestCommand):
-    """http://bit.ly/1T0dwvG
-    """
-    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.tox_args = None
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import tox
-        import shlex
-        args = self.tox_args
-        if args:
-            args = shlex.split(self.tox_args)
-        errno = tox.cmdline(args=args)
-        sys.exit(errno)
+requirements_without_comments = [
+    line for line in requirements_raw if line and not line.startswith('#')
+]
 
 
 setup(
     name="taskhuddler",
     version=VERSION,
     description="taskcluster-client wrapper",
-    author="Simon Fraser",
-    author_email="sfraser@mozilla.com",
-    url="https://github.com/srfraser/taskhuddler",
-    packages=["taskhuddler", "taskhuddler.test"],
+    author='Mozilla Release Engineering',
+    author_email='release+python@mozilla.com',
+    url="https://github.com/mozilla-releng/taskhuddler",
+    packages=find_packages(),
     package_data={"": ["version.json"]},
     include_package_data=True,
     zip_safe=False,
     license="MPL 2.0",
-    install_requires=reqs,
-    tests_require=tests_require,
-    cmdclass={'test': Tox},
+    install_requires=requirements_without_comments,
     classifiers=(
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ),
 )
