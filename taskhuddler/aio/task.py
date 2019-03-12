20135ee46757aed8a746417f628fd6bb07a31af2
Simon Fraser <srfraser@users.noreply.github.com>
2019-03-12 09:26:07 +0000

    Task refactor (#16)
    Use mixins to improve Task classes

--- taskhuddler/aio/task.py
+++ taskhuddler/aio/task.py
@@ -4,27 +4,11 @@ import logging
 
 from asyncinit import asyncinit
 from taskcluster.aio import Queue
-from taskhuddler.task import Task as SyncTask
 from taskhuddler.task import TaskDefinition as SyncTaskDefinition
 from taskhuddler.task import TaskStatus as SyncTaskStatus
 from taskhuddler.utils import tc_options
 
 log = logging.getLogger(__name__)
-
-
-@asyncinit
-class Task(SyncTask):
-    """Helper class for dealing with Tasks, asyncio version."""
-
-    async def __init__(self, json=None, task_id=None, queue=None):
-        """init."""
-        self.queue = queue
-        if not self.queue:
-            self.queue = Queue(tc_options())
-        if not json:
-            json = dict()
-        self.task = await TaskDefinition(json=json.get('task'), task_id=task_id, queue=self.queue)
-        self.status = await TaskStatus(json=json.get('status'), task_id=task_id, queue=self.queue)
 
 
 @asyncinit
@@ -37,7 +21,7 @@ class TaskDefinition(SyncTaskDefinition):
         if task_id:
             self.task_id = task_id
         if json:
-            self._json = json.get('task', json)
+            self.def_json = json.get('task', json)
             return
         if not task_id:
             raise ValueError('No task definition or taskId provided')
@@ -47,7 +31,7 @@ class TaskDefinition(SyncTaskDefinition):
         await self._fetch_definition()
 
     async def _fetch_definition(self):
-        self._json = await self.queue.task(self.task_id)
+        self.def_json = await self.queue.task(self.task_id)
 
 
 @asyncinit
@@ -60,8 +44,8 @@ class TaskStatus(SyncTaskStatus):
             self.task_id = task_id
         if json:
             # We might be passed {'status': ... } or just the contents
-            self._json = json.get('status', json)
-            self.task_id = self._json['taskId']
+            self.status_json = json.get('status', json)
+            self.task_id = self.status_json['taskId']
             return
         if not task_id:
             raise ValueError('No task definition or taskId provided')
@@ -72,4 +56,36 @@ class TaskStatus(SyncTaskStatus):
 
     async def _fetch_status(self):
         json = await self.queue.status(self.task_id)
-        self._json = json.get('status', json)
+        self.status_json = json.get('status', json)
+
+
+@asyncinit
+class Task(TaskDefinition, TaskStatus):
+    """Helper class for dealing with Tasks, asyncio version."""
+
+    async def __init__(self, json=None, task_id=None, queue=None):
+        """init."""
+        if json:
+            self.def_json = json.get('task')
+            self.status_json = json.get('status')
+            self.task_id = self.status_json['taskId']
+            return
+
+        if task_id:
+            self.task_id = task_id
+        else:
+            raise ValueError('No task definition or taskId provided')
+        self.queue = queue
+        if not self.queue:
+            self.queue = Queue(tc_options())
+        if self.task_id:
+            await self._fetch_definition()
+            await self._fetch_status()
+
+    def __repr__(self):
+        """repr."""
+        return "<Task {}>".format(self.task_id)
+
+    def __str__(self):
+        """Str representation."""
+        return "<Task {}:{}>".format(self.task_id, self.state)
--- taskhuddler/task.py
+++ taskhuddler/task.py
@@ -7,101 +7,6 @@ from taskcluster import Queue
 from .utils import tc_options
 
 
-class Task(object):
-    """Collected information about a single task."""
-
-    def __init__(self, json=None, task_id=None, queue=None):
-        """init."""
-        self.queue = queue
-        if not self.queue:
-            self.queue = Queue(tc_options())
-        if not json:
-            json = dict()
-        self.task = TaskDefinition(json=json.get('task'), task_id=task_id, queue=self.queue)
-        self.status = TaskStatus(json=json.get('status'), task_id=task_id, queue=self.queue)
-    # Properties to extract commonly used values.
-
-    def __repr__(self):
-        """repr."""
-        return "<Task {}>".format(self.task_id)
-
-    def __str__(self):
-        """Str representation."""
-        return "<Task {}:{}>".format(self.task_id, self.state)
-
-    @property
-    def json(self):
-        """Reconstruct json as presented by listTaskGroup."""
-        rv = dict()
-        rv.update(self.status.json)
-        rv.update(self.task.json)
-        return rv
-
-    @property
-    def task_id(self):
-        """Extract taskId."""
-        return self.status.task_id
-
-    @property
-    def taskid(self):
-        """Taskid for compatibility."""
-        return self.task_id
-
-    @property
-    def label(self):
-        """Extract label."""
-        return self.task.label
-
-    @property
-    def state(self):
-        """Return current task state."""
-        return self.status.state
-
-    @property
-    def completed(self):
-        """Return True if this task has completed."""
-        return self.status.completed
-
-    @property
-    def scheduled(self):
-        """Return datetime of the task scheduled time."""
-        return self.status.scheduled
-
-    @property
-    def started(self):
-        """Return datetime of the most recent run's start."""
-        return self.status.started
-
-    @property
-    def resolved(self):
-        """Return datetime of the most recent run's finish time."""
-        return self.status.resolved
-
-    def run_durations(self):
-        """Return a list of timedelta objects, of run durations."""
-        return self.status.run_durations()
-
-    @property
-    def kind(self):
-        """Return the task's kind."""
-        return self.task.kind
-
-    @property
-    def scopes(self):
-        """Return a list of the scopes used, if any."""
-        return self.task.scopes
-
-    @property
-    def has_failures(self):
-        """Return True if this task has any run failures."""
-        return self.status.has_failures
-
-    @property
-    def name(self):
-        """Return the name of the task."""
-        return self.task.name
-
-
 class TaskDefinition:
     """Data and queries about a task definition."""
 
@@ -111,7 +16,7 @@ class TaskDefinition:
         if task_id:
             self.task_id = task_id
         if json:
-            self._json = json.get('task', json)
+            self.def_json = json.get('task', json)
             return
         if not task_id:
             raise ValueError('No task definition or taskId provided')
@@ -121,7 +26,7 @@ class TaskDefinition:
         self._fetch_definition()
 
     def _fetch_definition(self):
-        self._json = self.queue.task(self.task_id)
+        self.def_json = self.queue.task(self.task_id)
 
     def __repr__(self):
         """repr."""
@@ -134,29 +39,29 @@ class TaskDefinition:
     @property
     def json(self):
         """Return json as originally presented."""
-        return {'task': self._json}
+        return {'task': self.def_json}
 
     @property
     def label(self):
         """Extract label."""
-        return self._json.get('tags', {}).get(
-            'label', self._json.get('metadata').get('name', '')
+        return self.def_json.get('tags', {}).get(
+            'label', self.def_json.get('metadata').get('name', '')
         )
 
     @property
     def kind(self):
         """Return the task's kind."""
-        return self._json['tags'].get('kind', '')
+        return self.def_json['tags'].get('kind', '')
 
     @property
     def scopes(self):
         """Return a list of the scopes used, if any."""
-        return self._json.get('scopes', [])
+        return self.def_json.get('scopes', [])
 
     @property
     def name(self):
         """Return the name of the task."""
-        return self._json['metadata']['name']
+        return self.def_json['metadata']['name']
 
 
 class TaskStatus:
@@ -168,8 +73,8 @@ class TaskStatus:
             self.task_id = task_id
         if json:
             # We might be passed {'status': ... } or just the contents
-            self._json = json.get('status', json)
-            self.task_id = self._json['taskId']
+            self.status_json = json.get('status', json)
+            self.task_id = self.status_json['taskId']
             return
         if not task_id:
             raise ValueError('No task definition or taskId provided')
@@ -180,7 +85,7 @@ class TaskStatus:
 
     def _fetch_status(self):
         json = self.queue.status(self.task_id)
-        self._json = json.get('status', json)
+        self.status_json = json.get('status', json)
 
     def __repr__(self):
         """repr."""
@@ -193,17 +98,17 @@ class TaskStatus:
     @property
     def json(self):
         """Return json as originally presented."""
-        return {'status': self._json}
+        return {'status': self.status_json}
 
     @property
     def state(self):
         """Return current task state."""
-        return self._json.get('state', '')
+        return self.status_json.get('state', '')
 
     @property
     def has_failures(self):
         """Return True if this task has any run failures."""
-        return len([r for r in self._json.get('runs', list()) if r.get('state') in ['failed', 'exception']]) > 0
+        return len([r for r in self.status_json.get('runs', list()) if r.get('state') in ['failed', 'exception']]) > 0
 
     @property
     def completed(self):
@@ -217,9 +122,9 @@ class TaskStatus:
 
     def _extract_date(self, run_field, run_id=-1):
         """Return datetime of the given field in the task runs."""
-        if not self._json.get('runs'):
+        if not self.status_json.get('runs'):
             return
-        field_data = self._json['runs'][run_id].get(run_field)
+        field_data = self.status_json['runs'][run_id].get(run_field)
         if not field_data:
             return
         return dateutil.parser.parse(field_data)
@@ -246,9 +151,52 @@ class TaskStatus:
         durations = list()
         if not self.json['status'].get('runs'):
             return durations
-        for run in self._json.get('runs', list()):
+        for run in self.status_json.get('runs', list()):
             started = run.get('started')
             resolved = run.get('resolved')
             if started and resolved:
                 durations.append(dateutil.parser.parse(resolved) - dateutil.parser.parse(started))
         return durations
+
+
+class Task(TaskDefinition, TaskStatus):
+    """Collected information about a single task."""
+
+    def __init__(self, json=None, task_id=None, queue=None):
+        """init."""
+        if json:
+            self.def_json = json.get('task')
+            self.status_json = json.get('status')
+            self.task_id = self.status_json['taskId']
+            return
+        if task_id:
+            self.task_id = task_id
+        else:
+            raise ValueError('No task definition or taskId provided')
+        self.queue = queue
+        if not self.queue:
+            self.queue = Queue(tc_options())
+        if self.task_id:
+            self._fetch_definition()
+            self._fetch_status()
+
+    def __repr__(self):
+        """repr."""
+        return "<Task {}>".format(self.task_id)
+
+    def __str__(self):
+        """Str representation."""
+        return "<Task {}:{}>".format(self.task_id, self.state)
+
+    @property
+    def taskid(self):
+        """Compatibility wrapper."""
+        return self.task_id
+
+    @property
+    def json(self):
+        """Reconstruct json as presented by listTaskGroup."""
+        return {
+            'task': self.def_json,
+            'status': self.status_json,
+        }
